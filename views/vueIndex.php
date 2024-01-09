67d8890f44a34d52f9eca512fc4da36827487a89
TheoDeguin <105062188+TheoDeguin@users.noreply.github.com>
2024-01-09 08:31:50 +0100

    [TP6]3.0

--- views/vueIndex.php
+++ views/vueIndex.php
@@ -8,3 +8,26 @@
 
 <h2>Pokémon by Non-Existing ID:</h2>
 <?php var_dump($nonExistingPokemon); ?>
+
+<table>
+    <tr>
+        <td>ID</td>
+        <td>Pokémon</td>
+        <td>Description</td>
+        <td>Types</td>
+        <td>Image</td>
+    </tr>
+    <?php
+    foreach($allPokemon as $pokemon){
+        echo("
+            <tr>
+                <td>$pokemon[idPokemon]</td>
+                <td>$pokemon[nomEspece]</td>
+                <td>$pokemon[description]</td>
+                <td>$pokemon[typeOne] $pokemon[typeTwo]</td>
+                <td><img src=$pokemon[urlImg] alt=$pokemon[nomEspece]></td>
+            </tr>
+        ");
+    }
+    ?>
+</table>
